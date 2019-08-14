910cea0939c157c49e9af56fbef0d0a6bec9cc24
DMonget <david.monget@gmail.com>
2019-08-14 09:50:19 +0200

    Changed Hello to parameterized version.

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world!"
+	got := Hello("David")
+	want := "Hello, David!"
 
 	if got != want {
 		t.Errorf("got %q, but wanted %q", got, want)
